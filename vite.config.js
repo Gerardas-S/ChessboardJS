3afc11a2520d9add81ca792816be9670d10d7307
Gerardas-S <114411595+Gerardas-S@users.noreply.github.com>
2022-11-25 01:05:52 +0200

    HH

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+export default {
+    // config options
+    define: {
+        // By default, Vite doesn't include shims for NodeJS/
+        // necessary for segment analytics lib to work
+        global: {},
+      }
+  }
